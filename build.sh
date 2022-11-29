b71eaa6772de1296bc98942886bd370631a166b4
Josh Attenberg <josh.attenberg@gmail.com>
2022-11-29 11:55:11 -0500

    update

--- build.sh
+++ build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
+# i use this to build a useful virtualenv for local development.
+# i use anaconda as a base (i think with py 3.8 at the moment)
 
 set -e
 
-if ! command -v virtualenv &> /dev/null 
+py=$(which python)
+
+if ! command -v virtualenv &> /dev/null
 then
     echo "virtualenv could not be found"
-    pip install pipx;
+    pip install virtualenv;
 else
   venv_path=$(which virtualenv);
   echo "using venv located at $venv_path"
 fi
 
 venv="venv"
-echo "making virtualenv: $venv"
+echo "making virtualenv $venv using python interpreter: $py"
 
-virtualenv $venv
+virtualenv --python $py $venv
 
-$venv/bin/pip install --upgrade pip
-#$venv/bin/pip install --upgrade numpy pandas sklearn statsmodels numba
-#$venv/bin/pip install -Iv cython==0.29.17
-$venv/bin/pip install -e .
+$venv/bin/pip install --upgrade pip pytest pip-tools
+$venv/bin/pip install -r requirements.txt
+
